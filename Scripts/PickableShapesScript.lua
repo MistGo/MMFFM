881fa9d4c520b51e8644dd819a098a383c61992c
Georgiy <116843389+MistGo@users.noreply.github.com>
2024-03-25 22:23:47 +0300

    Add files via upload

--- Scripts/PickableShapesScript.lua
+++ Scripts/PickableShapesScript.lua
@@ -1,6 +1,6 @@
 --[[
     Script created by MistGo
-    Updated 10.11.2023
+    Updated 25.03.2024
 ]]
 
 PickableShapes = class()
@@ -19,17 +19,28 @@ function PickableShapes:server_onRefresh()
     print("Pickable Shapes refreshed.")
 end
 
-function PickableShapes:sv_tunnel(player)
-    self.network:sendToClient(player.player, "cl_getItem")
+function PickableShapes:sv_tunnel(args)
+    self.network:sendToClient(args.player, "cl_getItem", {hotbar_page = args.hotbar_page})
 end
 
 function PickableShapes:sv_changeItem(args)
-    local current_slot, hotbar, inventory, uuid = args.slot, args.hotbar, args.inventory, args.uuid
+    local current_slot, max_slot, hotbar, hotbar_page, inventory, uuid = args.slot, 9, args.hotbar, args.hotbar_page, args.inventory, args.uuid
+
+    print(hotbar_page)
+
+    if hotbar_page == nil then
+    elseif hotbar_page == 2 then
+        current_slot = current_slot+10
+        max_slot = 19
+    elseif hotbar_page == 3 then
+        current_slot = current_slot+20
+        max_slot = 29
+    elseif hotbar_page > 3 or hotbar_page < 1 then else end
 
     if not isSurvival then -- Creative
         local cur_item = hotbar:getItem(current_slot)
         local found = false
-        for slot = 0, 9 do
+        for slot = 0, max_slot do
             local items = hotbar:getItem(slot)
             if items and (items.uuid == uuid) then
                 sm.container.beginTransaction()
@@ -57,15 +68,16 @@ function PickableShapes:sv_changeItem(args)
     end
 end
 
-function PickableShapes:cl_getItem()
+function PickableShapes:cl_getItem(args)
     local slot = sm.localPlayer.getSelectedHotbarSlot()
     local container = isSurvival and sm.localPlayer.getInventory() or sm.localPlayer.getHotbar()
+    local hotbar_page = args.hotbar_page
     local bool, result = sm.localPlayer.getRaycast(5, sm.localPlayer.getRaycastStart(), sm.localPlayer.getDirection())
 
     if bool and (result.type == "body") then
         local shape = result:getShape().uuid
         if sm.exists(shape) then
-            local args = { hotbar = not isSurvival and container or nil, inventory = isSurvival and container or nil, slot = slot, uuid = shape }
+            local args = { hotbar = not isSurvival and container or nil, inventory = isSurvival and container or nil, slot = slot, uuid = shape, hotbar_page = not isSurvival and hotbar_page or nil }
             self.network:sendToServer("sv_changeItem", args)
         end
     end
@@ -76,7 +88,7 @@ if not commandsBind then
     local function bindCommandHook(command, params, callback, help)
         oldBindCommand(command, params, callback, help)
         if not added then
-            oldBindCommand("/get", {}, "cl_onChatCommand", "Aim at the desired block and enter the following command to get it in the active hotbar slot.")
+            oldBindCommand("/get", { { "int", "hotbar_page", true } }, "cl_onChatCommand", "Aim at the desired block and enter the following command to get it in the active hotbar slot. You can also choose the hotbar page (default 1).")
             added = true
         end
     end
@@ -87,7 +99,7 @@ if not commandsBind then
     local function worldEventHook(world, callback, params)
         if params then
             if params[1] == "/get" then
-                sm.event.sendToTool(PickableShapes.tool, "sv_tunnel", { player = params.player })
+                sm.event.sendToTool(PickableShapes.tool, "sv_tunnel", { player = params.player, hotbar_page = (params[2] or 1) })
                 return
             end
         end
