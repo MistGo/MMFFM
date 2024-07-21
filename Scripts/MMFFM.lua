e749e9f567a0511dcc033449b52f8b36c1ffbf1a
MistGo <116843389+MistGo@users.noreply.github.com>
2024-07-21 11:32:36 +0300

    update

--- Scripts/MMFFM.lua
+++ Scripts/MMFFM.lua
@@ -4,46 +4,39 @@
 ]]
 
 MMFFM = class()
-local isSurvival = nil
-local defaultHotbarPage = 1
 
 function MMFFM:server_onCreate()
     MMFFM.tool = self.tool
-    isSurvival = sm.game.getLimitedInventory()
-
-    local GameMode = isSurvival and "Survival" or "Creative"
-    print("MMFFM for " .. GameMode .. " loaded.")
-    sm.gui.chatMessage("#0098EA[MMFFM]#FFFFFF Use #0098EA/get #FFFFFFto obtain the required block without opening the inventory. You can also switch between hotbar pages by adding page number: /get #0098EA[1/2/3]#FFFFFF")
 end
 
 function MMFFM:server_onRefresh()
-    MMFFM.tool = self.tool
-    isSurvival = sm.game.getLimitedInventory()
+    self:server_onCreate()
 end
 
-function MMFFM:sv_changeItem(args)
-    local min_slot, current_slot, hotbar, hotbar_page, inventory, uuid = 0, args.slot, args.hotbar, args.hotbar_page, args.inventory, args.uuid
+function MMFFM:sv_changeItem(params)
+    local min_slot, current_slot, uuid = 0, params.slot, params.uuid
+    local hotbar, hotbar_page = params.hotbar, params.hotbar_page
+    local inventory, gamemode = params.inventory, params.gamemode
 
-    if hotbar_page == 2 then
-        min_slot = 10
-        current_slot = current_slot + 10
-    elseif hotbar_page == 3 then
-        min_slot = 20
-        current_slot = current_slot + 20
-    end
+    min_slot = (hotbar_page - 1) * 10
+    current_slot = current_slot + min_slot
 
-    if not isSurvival then -- Creative
+    if not gamemode then -- Creative
         local cur_item = hotbar:getItem(current_slot)
         local isFound, isEmpty = false, false
-        local emptySlot, emptyUuid = 0, sm.uuid.new("00000000-0000-0000-0000-000000000000")
+        local emptySlot, emptyUuid = 0, sm.uuid.getNil()
         
         for slot = min_slot, (hotbar:getSize() - 1) do
             local items = hotbar:getItem(slot)
+
             if items and (items.uuid == uuid) then
                 sm.container.beginTransaction()
-                if isEmpty then sm.container.swap(hotbar, current_slot, hotbar, emptySlot) end
+                if isEmpty then 
+                    sm.container.swap(hotbar, current_slot, hotbar, emptySlot) 
+                end
                 sm.container.swap(hotbar, slot, hotbar, current_slot)
                 sm.container.endTransaction()
+
                 isFound = true
                 break
             elseif items and not isEmpty and (items.uuid == emptyUuid) and (cur_item.uuid ~= emptyUuid) then
@@ -74,42 +67,62 @@ function MMFFM:sv_changeItem(args)
     end
 end
 
-function MMFFM:cl_getItem(params)
-    local slot = sm.localPlayer.getSelectedHotbarSlot()
-    local hotbar_page = params.hotbar_page
-    local container = isSurvival and sm.localPlayer.getInventory() or sm.localPlayer.getHotbar()
-    local bool, result = sm.localPlayer.getRaycast(6, sm.localPlayer.getRaycastStart(), sm.localPlayer.getDirection())
+function MMFFM:client_onCreate()
+    self.cl = {
+        hotbarPage = 1,
+        isSurvival = sm.game.getLimitedInventory()
+    }
 
-    local uuid = sm.uuid.new("00000000-0000-0000-0000-000000000000")
+    sm.gui.chatMessage("#0098EA[MMFFM]#FFFFFF Use #0098EA/get #FFFFFFto obtain the required block without opening the inventory. You can also switch between hotbar pages by adding page number: /get #0098EA[1/2/3]#FFFFFF")
+end
+
+function MMFFM:client_onRefresh()
+    self:client_onCreate()
+end
+
+function MMFFM:client_onFixedUpdate()
+    if self.cl.isSurvival ~= sm.game.getLimitedInventory() then
+        print("[MMFFM] Changed gamemode for: ".. sm.localPlayer.getPlayer().name)
+        self.cl.isSurvival = sm.game.getLimitedInventory()
+    end
+end
+
+function MMFFM:cl_getItem(hotbarPage)
+    local slot = sm.localPlayer.getSelectedHotbarSlot()
+    local container = self.cl.isSurvival and sm.localPlayer.getInventory() or sm.localPlayer.getHotbar()
+    local hit, hitRes = sm.localPlayer.getRaycast(5)
+    local hotbar_page = hotbarPage or self.cl.hotbarPage
+
+    local uuid = sm.uuid.getNil()
     local particle_effect = nil
 
-    if bool then
-        local item = result:getShape() or result:getJoint()
+    if hit then
+        local item = hitRes:getShape() or hitRes:getJoint()
+
         if sm.exists(item) then
             uuid = item.uuid
-            particle_effect = (result.type == "body") and "p_tool_multiknife_refine_hit_metal" or "p_barrier_impact"
+            particle_effect = (hitRes.type == "body") and "p_tool_multiknife_refine_hit_metal" or "p_barrier_impact"
         end
     
         if particle_effect then
-            sm.particle.createParticle(particle_effect, result.pointWorld + (result.normalWorld * 0.03))
+            sm.particle.createParticle(particle_effect, hitRes.pointWorld + (hitRes.normalWorld * 0.03))
         end
-
-        local args = {
-            hotbar = not isSurvival and container or nil,
-            inventory = isSurvival and container or nil,
+        
+        local params = {
+            hotbar = not self.cl.isSurvival and container or nil,
+            inventory = self.cl.isSurvival and container or nil,
             slot = slot,
             uuid = uuid,
-            hotbar_page = not isSurvival and hotbar_page or nil
+            hotbar_page = not self.cl.isSurvival and hotbar_page or 1,
+            gamemode = self.cl.isSurvival
         }
-        self.network:sendToServer("sv_changeItem", args)
-    end 
+        self.network:sendToServer("sv_changeItem", params)
+        self.cl.hotbarPage = hotbarPage or self.cl.hotbarPage
+    end
 end
 
 function MMFFM:sv_tunnel(params)
-    if params.hotbar_page ~= nil then
-        defaultHotbarPage = params.hotbar_page
-    end
-    self.network:sendToClient(params.player, "cl_getItem", { hotbar_page = params.hotbar_page })
+    self.network:sendToClient(params.player, "cl_getItem", params.hotbar_page)
 end
 
 if not commandsBind then
@@ -117,7 +130,7 @@ if not commandsBind then
     local function bindCommandHook(command, params, callback, help)
         oldBindCommand(command, params, callback, help)
         if not added then
-            oldBindCommand("/get", { { "int", "hotbar_page", true } }, "cl_onChatCommand", "Use that command to obtain the required block without opening the inventory. You can also switch between hotbar pages by adding page number: /get #0098EA[1/2/3]#FFFFFF")
+            oldBindCommand("/get", { { "int", "hotbar_page", true } }, "cl_onChatCommand", "")
             added = true
         end
     end
@@ -126,7 +139,7 @@ if not commandsBind then
     local function worldEventHook(world, callback, params)
         if params then
             if params[1] == "/get" then
-                sm.event.sendToTool(MMFFM.tool, "sv_tunnel", { player = params.player, hotbar_page = (params[2] or defaultHotbarPage) })
+                sm.event.sendToTool(MMFFM.tool, "sv_tunnel", {player = params.player, hotbar_page = params[2]})
                 return
             end
         end
